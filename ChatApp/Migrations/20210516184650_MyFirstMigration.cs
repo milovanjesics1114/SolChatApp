5760f1219d3c3718ced73249f0607ac935257c98
milovanjesics1114 <milovanjesics1114@users.noreply.github.com>
2021-05-16 21:52:07 +0200

    Database connection string, CRUD

---
+++ ChatApp/Migrations/20210516184650_MyFirstMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatApp.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatAppModel",
+                columns: table => new
+                {
+                    korisnikID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    korisnik_ime = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    korisnik_korisnicko_ime = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    korisnik_sifra = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    korisnik_email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    korisnik_logo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    korisnik_status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatAppModel", x => x.korisnikID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatAppModel");
+        }
+    }
+}
